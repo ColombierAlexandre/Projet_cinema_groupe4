752781e11a72808bf983004dcfba16ee67237a17
Samir31000 <kamalsamir@outlook.fr>
2023-05-09 11:09:11 +0200

    test

--- Cinema_groupe4_front/cinema_front/src/app/app-routing.module.ts
+++ Cinema_groupe4_front/cinema_front/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListeFilmComponent } from './composant/liste-film/liste-film.component';
+import { ValiderTicketComponent } from './composant/valider-ticket/valider-ticket.component';
 
 const routes: Routes = [
-
-
-
+  {path:"listFilms",component:ListeFilmComponent},
+  {path:"listFilms",component:ValiderTicketComponent},
   
+  {path:"",redirectTo: '/listFilms',pathMatch:'full'}
 ];
 
 @NgModule({
